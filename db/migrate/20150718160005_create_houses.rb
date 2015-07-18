764fcf46f0dab5db7075023ba8c3e8a28c6c1d9c
mirkoc <mirko.cerovac@gmail.com>
2015-07-18 18:00:52 +0200

    First commit.

---
+++ db/migrate/20150718160005_create_houses.rb
@@ -0,0 +1,15 @@
+class CreateHouses < ActiveRecord::Migration
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :postal_code
+      t.string :ovner
+
+      t.timestamps null: false
+    end
+  end
+end
